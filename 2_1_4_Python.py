7b9186a04aa38c36fc9d75a7b956f5cd88ae697b
Hector Rifo <54448892+hrifo@users.noreply.github.com>
2021-09-25 09:50:27 -0300

    Add files via upload

---
+++ 2_1_4_Python.py
@@ -0,0 +1,8 @@
+primer_numero = int(input("Ingrese un numero: "))
+segundo_numero = int(input("Ingrese un numero: "))
+
+numero_decimal=3.6
+
+
+print("La suma de los numeros es: ", primer_numero + segundo_numero)
+print("La resta de los numeros es: ", primer_numero - segundo_numero)
